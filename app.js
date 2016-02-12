6318d7cff7e168cb676011008f71ef0e665e08f0
rptorres22 <rtorres22@gmail.com>
2016-02-12 10:54:21 -0800

    port changes

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 var routes = require("./routes/routes.js")(app);
 
-var server = app.listen(3000, function() {
+var server = app.listen(app.get('port'), function() {
 	console.log("Listening on port %s...", server.address().port);
 });
 
